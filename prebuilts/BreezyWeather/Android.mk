8bc3aafb2c7cab34874416f9137b730a99ae70cc
Anna-Sophie Zaitsewa <toddot@protonmail.com>
2024-05-02 22:56:55 +0200

    [prebuilts]: Use BreezyWeather as weather app

---
+++ prebuilts/BreezyWeather/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := BreezyWeather
+LOCAL_SRC_FILES := BreezyWeather.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OPTIONAL_USES_LIBRARIES += \
+		androidx.window.extensions \
+		androidx.window.sidecar
+include $(BUILD_PREBUILT)
